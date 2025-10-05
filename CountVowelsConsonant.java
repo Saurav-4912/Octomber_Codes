b115fd75d8e7a8837dfc5b95815a4fc94b1be234
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-05 09:46:25 +0530

    Count vowles and consonant

--- CountVowelsConsonant.java
+++ CountVowelsConsonant.java
@@ -10,11 +10,17 @@ public class CountVowelsConsonant{
 
 		for(int i = 0; i < str.length(); i++){
 
-			if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+			char ch = str.charAt(i);
+
+			if(ch >= 'a' && ch <= 'z'){
+				if(ch == 'a' || ch == 'e' || ch== 'i' || ch == 'o' || ch == 'u'){
 				vowel++;
 			}else{
 				consonant++;
 			}
+			}
+
+			
 		}
 
 		System.out.println("Vowels : " + vowel);
