457b30c921acd970a089e5d1b801bd9106c664c0
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-11 06:57:07 +0530

    Rotate array (left rotation)

---
+++ RotateArray.java
@@ -0,0 +1,29 @@
+public class RotateArray {
+
+	public static void main(String[] args)
+	{
+
+		int[] arr = {1,2,3,4,5};
+
+		int rotateBy = 2;
+
+
+		    for(int i = 0; i < rotateBy; i++){
+
+		    	int temp = arr[0];
+
+		    	for(int j = 1; j < arr.length; j++){
+
+		    		arr[j-1] = arr[j];
+		    	}
+
+		    	arr[arr.length-1] = temp;
+		    }
+
+		    for(int k = 0; k < arr.length; k++){
+
+		    	System.out.println(arr[k]);
+		    }
+
+	}
+}
