281e086d10c10c9e513a40e44ee130fdb7587cd4
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-14 12:51:08 +0530

    Sum of digit of given number

---
+++ SumOfDigit.java
@@ -0,0 +1,22 @@
+public class SumOfDigit {
+
+	public static void main(String[] args){
+
+		int number = 99999;
+
+		int originalNumber = number;
+
+		int sum = 0;
+
+		while(number > 0){
+
+			int remainder = number % 10;
+
+			sum += remainder;
+
+			number = number / 10;
+		}
+
+		System.out.println("sum of digit of given number is : " + sum);
+	}
+}
