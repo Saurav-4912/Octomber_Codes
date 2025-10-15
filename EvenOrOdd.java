8acadbbbf580a2194c43f4bbf69947263782aa3e
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-15 15:03:27 +0530

    Check given number is even or odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+public class EvenOrOdd {
+
+	public static void main(String[] args){
+
+		int number = 21;
+
+		if(number % 2 == 0){
+			System.out.println(number + " is even number");
+		}else{
+			System.out.println(number + " is odd number");
+		}
+	}
+}
