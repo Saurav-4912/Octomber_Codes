3aa92102fc715d8c289e2e00a402edad9813c8d3
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-17 10:46:43 +0530

    Check whether a number is positive, negative, or zero

---
+++ CheckNumberType.java
@@ -0,0 +1,16 @@
+public class CheckNumberType {
+
+	public static void main(String[] args){
+
+		int number = -100;
+
+
+		if(number > 0){
+			System.out.println("Number is positive");
+		}else if(number < 0){
+			System.out.println("Number is negative");
+		}else{
+			System.out.println("Number is zero");
+		}
+	}
+}
