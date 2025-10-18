6655858024219d1fd228de08cd2aa52969dd2ddc
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-18 12:54:02 +0530

    Move all zeros to the end of an array

---
+++ MoveZerosToEnd.java
@@ -0,0 +1,24 @@
+public class MoveZerosToEnd{
+
+	public static void main(String[] args){
+
+		int[] arr = {0, 1, 0, 3, 12};
+
+		int j = 0;
+
+		for(int i = 0; i < arr.length; i++){
+
+			if(arr[i] != 0){
+
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				j++;
+			}
+		}
+
+		for(int num : arr){
+			System.out.print(num + " ");
+		}
+	}
+}
