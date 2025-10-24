c65a12c94719ed291c92b89d56b5bf711b9251ad
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-24 08:18:31 +0530

    Copy one string into another string

---
+++ CopyString.java
@@ -0,0 +1,14 @@
+public class CopyString{
+
+	public static void main(String[] args) {
+
+		String str = "I love india";
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(str);
+
+		System.out.println(sb);
+		
+	}
+}
