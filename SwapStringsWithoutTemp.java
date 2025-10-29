463011bd42a78acbe5a9c64c4cc4074d9633f2bb
Saurav Pawar <sauravpawar46@gmail.com>
2025-10-29 06:51:16 +0530

    Swap string without third variable

---
+++ SwapStringsWithoutTemp.java
@@ -0,0 +1,22 @@
+public class SwapStringsWithoutTemp {
+
+	public static void main(String[] args) {
+		
+
+		String str1 = "Hello";
+
+		String str2 = "World";
+
+		str1 = str1 + str2;
+
+		// str1 = HelloWorld
+
+		str2 = str1.substring(0, str1.length() - str2.length());  // str1.substring(0,5);
+
+
+		str1 = str1.substring(str2.length());
+
+		System.out.println(str1);
+		System.out.println(str2);
+	}
+}
